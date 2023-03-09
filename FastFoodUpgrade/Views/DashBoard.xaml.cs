c597c6a76086598ae2c7d924ea64671c0d5ab8c3
Phong <thanhphong17160@gmail.com>
2023-03-09 08:29:10 +0700

    Add project files.

---
+++ FastFoodUpgrade/Views/DashBoard.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FastFoodUpgrade.Views
+{
+    /// <summary>
+    /// Interaction logic for DashBoard.xaml
+    /// </summary>
+    public partial class DashBoard : UserControl
+    {
+        public DashBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewItem_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ListViewItem item = sender as ListViewItem;
+            if (item != null)
+            {
+                int index = mylistview.Items.IndexOf(item.DataContext);
+                // do something with the index...
+            }
+        }
+
+        private void ListViewItem_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FastFoodUpgrade/Views/ThicknessExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FastFoodUpgrade.Views
+{
+    public static class ThicknessExtensions
+    {
+        public static readonly DependencyProperty BottomProperty =
+            DependencyProperty.RegisterAttached(
+                "Bottom",
+                typeof(double),
+                typeof(ThicknessExtensions),
+                new FrameworkPropertyMetadata(
+                    0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsParentArrange |
+                    FrameworkPropertyMetadataOptions.AffectsParentMeasure),
+                value => (double)value >= 0);
+
+        public static double GetBottom(DependencyObject obj)
+        {
+            return (double)obj.GetValue(BottomProperty);
+        }
+
+        public static void SetBottom(DependencyObject obj, double value)
+        {
+            obj.SetValue(BottomProperty, value);
+        }
+    }
+
+}
