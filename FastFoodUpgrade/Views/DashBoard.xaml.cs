160e9644cd8661a9c3b73d63c75030b08093fce4
Phong <thanhphong17160@gmail.com>
2023-03-19 14:02:28 +0700

    update

--- FastFoodUpgrade/Views/DashBoard.xaml.cs
+++ FastFoodUpgrade/Views/DashBoard.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,21 +24,35 @@ namespace FastFoodUpgrade.Views
         public DashBoard()
         {
             InitializeComponent();
+            //mylistview.ItemsSource = new List<string> { "Item 1", "Item 2", "Item 3" };
         }
 
         private void ListViewItem_PreviewMouseUp(object sender, MouseButtonEventArgs e)
         {
-            ListViewItem item = sender as ListViewItem;
-            if (item != null)
-            {
-                int index = mylistview.Items.IndexOf(item.DataContext);
-                // do something with the index...
-            }
+            //ListViewItem item = sender as ListViewItem;
+            //if (item != null)
+            //{
+            //    int index = mylistview.Items.IndexOf(item.DataContext);
+            //    // do something with the index...
+            //}
         }
 
-        private void ListViewItem_MouseMove(object sender, MouseEventArgs e)
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
         {
+            if (sender is Grid grid)
+            {
+                grid.BeginStoryboard((Storyboard)FindResource("scaleUp"));
+            }
+        }
+        
 
+
+        private void ListViewItem_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Grid grid)
+            {
+                grid.BeginStoryboard((Storyboard)FindResource("scaleDown"));
+            }
         }
     }
 }
---
+++ FastFoodUpgrade/Views/Product.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FastFoodUpgrade.Views
+{
+    /// <summary>
+    /// Interaction logic for Product.xaml
+    /// </summary>
+    public partial class Product : Page
+    {
+        public Product()
+        {
+            InitializeComponent();
+            mylistview.ItemsSource = new List<string> { "akl", "akl", "akl", "akl", "akl", "akl", "akl", "akl", "akl", "akl", "akl" };
+        }
+        string SelectedStringItem = "";
+        Tuple<int, Grid> DraggedObject = null;
+        private void ListView_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if(!(sender is ListView))
+                {
+                    return;
+                }
+                var listView = sender as ListView;
+                var item = FindAncestor<Grid>((DependencyObject)e.OriginalSource);
+
+                if (item != null)
+                {
+                    var index = listView.Items.IndexOf(item.DataContext);
+                    SelectedStringItem = (string)item.DataContext;
+                    DraggedObject = new Tuple<int, Grid>(index, item);
+                    DragDrop.DoDragDrop(listView, new Tuple<int, Grid>(index, item), DragDropEffects.Move);
+                }
+            }
+        }
+
+        private static T FindAncestor<T>(DependencyObject current) where T : DependencyObject
+        {
+            while (current != null && !(current is T))
+            {
+                current = VisualTreeHelper.GetParent(current);
+            }
+        
+            return current as T;
+        }
+    }
+}
