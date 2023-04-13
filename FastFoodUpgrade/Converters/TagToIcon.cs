a28cd477632dcc3b201bde5e6ea78b75717d2903
Phong <thanhphong17160@gmail.com>
2023-04-13 10:27:30 +0700

    Update radio button to dashboard

---
+++ FastFoodUpgrade/Converters/TagToIcon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using FontAwesome.WPF;
+
+namespace FastFoodUpgrade.Converters
+{
+    public class TagToIcon : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is string IconName)
+            {
+                FontAwesomeIcon Icon;
+                if(Enum.TryParse(IconName,out Icon))
+                {
+                    return Icon;
+                }
+            }
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
